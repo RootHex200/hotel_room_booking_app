63221140116eec30fa539dff7d391f5fd3d3eac5
RootHex200 <sabiturrahman60@gmail.com>
2024-03-04 23:10:09 +0600

    add app colors and strings

---
+++ lib/src/core/values/colors/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color yellowColor = Color(0xFFFFD700);
+  static const Color primaryColor = Color(0xFF007EF2);
+  static const Color blackTextColor=Color(0xFF000000);
+  static const Color whiteTextColor = Color(0xFFFFFFFF);
+  static const Color grayTextColor=Color(0xFF7F7F7F);
+  static const Color whiteBackgroundColor=Color(0xFFF5FAFE);
+  static const Color whiteContainerColor=Color(0xFFD9D9D9);
+  static const Color textFieldBorderColor=Color(0xFF6C6868);
+  static const Color opacityPrimaryColor=Color(0xFF007EF2);
+}
---
+++ lib/src/core/values/strings/app_strings.dart
@@ -0,0 +1,46 @@
+class AppString {
+  static const String splashPageHeadLine="WanderStay";
+  static const String splashPageSubLine="Find Your Stay, Your Way";
+  static const String unleashYourInnerTraveller =
+      "Unleash Your Inner Traveller";
+  static const String unleashYourInnerTravellerSubLine =
+      "Your passport to a world of extraordinary hotel experiences. Join us today and unlock a realm of comfort, luxury, and adventure.";
+  static const String alreadyhaveanaccount = "Already have an account?";
+  static const String login = "Login";
+  static const String signup = "Signup";
+  static const String startExploring = "Start Exploring";
+  static const String create = "Create";
+  static const String account = "Accountg";
+  static const String signUpLongText =
+      "Fill your information below or register with your social account.";
+  static const String continueText = "Continue";
+  static const String orSignUpWith = "Or Signup with";
+  static const String welcome = "Welcome";
+  static const String back = "Back";
+  static const String verify = "Verify ";
+  static const String accountotp = "Account";
+  static const String receiveCode = "Didn’t receive code? ";
+  static const String resendAgain = "Resend again";
+  static const String congratulations = "Congratulations!!";
+  static const String congratulationsSubtitle = "Your hotel stay is secured. Counting down to your dream vacation!";
+  static const String goHome = "Go Home";
+  static const String viewEReciept = "View E Reciept";
+  static const String verificationText = "Please  enter the verification code sent to example@gmail.com";
+  static const String  forgotPassword = "Forgot Password?";
+   
+  static const String signinLongtext =
+      "We missed you! Login to continue your journey with us. ";
+
+  static const String payment = "Payment";
+  static const String totalPrice = "Total Price";
+  
+  static const String vat = "5% vst included";
+  static const String paymentMethod = "Payment Method";
+
+  static const String cardHolderName = "Card Holder Name";
+  static const String cardNumber = "Card Number";
+  static const String expiryDate = "Expiry Date";
+  static const String cvv = "CVV";
+  static const String payNow = "Pay Now";
+
+}

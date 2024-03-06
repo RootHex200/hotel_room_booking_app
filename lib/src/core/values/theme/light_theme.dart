3a4a7899e605336a2dd260b5896bdf899342242c
RootHex200 <sabiturrahman60@gmail.com>
2024-03-06 23:01:08 +0600

    Light Theme config added

---
+++ lib/src/core/values/theme/light_theme.dart
@@ -0,0 +1,10 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:hotel_room_booking/src/core/values/colors/app_colors.dart';
+
+final lightTheme=ThemeData(
+  primaryColor: AppColor.primaryColor,
+  scaffoldBackgroundColor: AppColor.whiteBackgroundColor,
+  useMaterial3: true,
+);
